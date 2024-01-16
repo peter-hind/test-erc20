c1ea183023775c96eb20c2e7b2ae3635f4872767
Peter Hind <peter.hind47@gmail.com>
2024-01-16 13:52:53 +1300

    add allowances test

--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -6,5 +6,35 @@ import {DeployPeteCoin} from "../script/DeployPeteCoin.s.sol";
 import {OurToken} from "../src/OurToken.sol";
 
 contract OurTokenTest is Test {
-    function setUp() public {}
+    OurToken public ourToken;
+    DeployPeteCoin public deployer;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    function setUp() public {
+        deployer = new DeployPeteCoin();
+        ourToken = deployer.run();
+
+        vm.prank(msg.sender);
+        ourToken.transfer(bob, STARTING_BALANCE);
+    }
+
+    function testBobBalance() public {
+        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE);
+    }
+
+    function testAllowances() public {
+        uint256 initialAllowance = 1000;
+        uint256 transferAmount = 500;
+        vm.prank(bob);
+        ourToken.approve(alice, initialAllowance);
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, transferAmount);
+
+        assertEq(ourToken.balanceOf(alice), transferAmount);
+        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
+    }
 }
